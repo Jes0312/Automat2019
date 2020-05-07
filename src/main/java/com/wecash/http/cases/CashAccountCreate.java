134b5f3c9374acdbe9b925fa209e48bead4a16a6
wecash <wuqiong@wecash.net>
2020-05-07 19:01:51 +0800

    修改usercreate，优化sql效率

--- src/main/java/com/wecash/http/cases/CashAccountCreate.java
+++ src/main/java/com/wecash/http/cases/CashAccountCreate.java
@@ -1,16 +1,9 @@
 package com.wecash.http.cases;
 
 import com.wecash.http.common.BaseProvider;
-
-
-
 import com.wecash.http.utils.DBUtils;
-
 import lombok.extern.slf4j.Slf4j;
-
 import org.testng.Assert;
-import org.testng.ITestContext;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import java.util.Map;
